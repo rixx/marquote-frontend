213cc15545aeecb6242b9260a38b5fd26416bbc7
Tobias Kunze <tobias.kunze@ax-semantics.com>
2016-01-25 22:56:28 +0100

    docker won't work despite same settings as the other project

--- marquote/marquote/settings.py
+++ marquote/marquote/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c3^4^4o4*k$-pl_&h7^#b3x*9j%%ces+a8m&9ys44v^ol*+a=@'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
