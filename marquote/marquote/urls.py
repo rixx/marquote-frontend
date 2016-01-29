b43403dcce01562386ef1f5f2bd1503065c943b4
Tobias Kunze <tobias.kunze@ax-semantics.com>
2016-01-29 23:44:52 +0100

    [shakespeare] added route to empty view

--- marquote/marquote/urls.py
+++ marquote/marquote/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-   # url(r'^shakespeare/', include('shakespeare.urls')),
+    url(r'^shakespeare/', include('shakespeare.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', include('main.urls')),
 ]
--- marquote/shakespeare/views.py
+++ marquote/shakespeare/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+
+class IndexView(View):
+    template_name = 'shakespeare/index.html'
+
+    def get(self, request):
+        return render(request, self.template_name, {})
