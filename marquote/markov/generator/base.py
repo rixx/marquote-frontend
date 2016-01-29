7a08fda03f3c56ffb95c1039f7ce3a9ff8bf77ba
Tobias Kunze <tobias.kunze@ax-semantics.com>
2016-01-29 23:10:07 +0100

    [markov] move lookahead validation to separate function

--- marquote/markov/generator/base.py
+++ marquote/markov/generator/base.py
@@ -8,12 +8,16 @@ class BaseGenerator:
     def generate(self, *args, lookahead=3 **kwargs):
         project = self.sentence_model.objects.first().project
         sentence = [project.start_word]
-
-        if lookahead < 1:
-            return 
-
-        if lookahead > project.max_lookahead:
-            # raise OverflowError('Lookahead larger than allowed ({} > {}).'.format(lookahead, project.max_lookahead))
-            # error handling
+        
+        if not self.is_valid_lookahead(lookahead, project):
+            # TODO: error handling
             return
 
+    def is_valid_lookahead(self, lookahead, project):
+        if not isinstance(lookahead, int):
+            return False
+
+        if not (1 < lookahead < project.max_lookahead):
+            return False
+
+        return True
